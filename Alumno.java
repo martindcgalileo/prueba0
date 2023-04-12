23442727925293f0dcd27a906d999ae07ca2702d
martindcgalileo <martindc@iesgalileo.es>
2023-04-12 10:35:22 +0200

    v2: * Alumno.java con metodos get * Programa.java prueba metodos get * .gitignore para ignorar los archivos .class

--- Alumno.java
+++ Alumno.java
@@ -9,6 +9,13 @@ class Alumno
       this.numMatricula=numMatricula;
       this.nombre=nombre;
    }
+   public int getNumMatricula(){
+      return numMatricula;
+   }
+   public String getNombre()
+   {
+      return nombre;
+   }
    @Override
    public String toString(){
       return "Alumno[ numMatricula="+numMatricula+", nombre="+nombre+"]";
--- Programa.java
+++ Programa.java
@@ -2,5 +2,7 @@ class Programa{
 public static void main(String[] args){
    Alumno a1=new Alumno(1,"Pepe");
    System.out.println(a1);
+   System.out.println("numero de matricula = "+a1.getNumMatricula());
+   System.out.println("nombre de alumno    = "+a1.getNombre());
 }
 }
